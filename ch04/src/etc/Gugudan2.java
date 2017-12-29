ad605fc0f8068d7bf1965120b89d5094c0aaed90
KOITT <KOITT@KOITT-PC14>
2017-12-29 15:18:45 +0900

    잉ㅠ 빠짐

--- ch04/src/etc/Gugudan2.java
+++ ch04/src/etc/Gugudan2.java
@@ -7,7 +7,7 @@ public class Gugudan2 {
 		
 		for (int i = 1; i < 10; i++) {
 			for (int j = 2; j < 10; j++) {
-				System.out.printf("%2d * %2d = %2d ", j, i, j*i);
+				System.out.printf("%2d *%2d = %2d ", j, i, j*i);
 			}
 			System.out.println();
 		}
